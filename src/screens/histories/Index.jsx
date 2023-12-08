8439ba0d15b6640a3f0a8eb74f3c751935a5c844
Angga Saputra <angsa0077@gmail.com>
2023-12-08 15:03:02 +0700

    add screen lamp, status, and chart history

--- src/screens/histories/Index.jsx
+++ src/screens/histories/Index.jsx
@@ -1,27 +1,130 @@
-import {View, Text, StyleSheet} from 'react-native';
+import {
+  SafeAreaView,
+  ScrollView,
+  View,
+  Text,
+  StyleSheet,
+  Dimensions,
+} from 'react-native';
 
-import React from 'react';
+// import hook
+import React, {useState, useEffect} from 'react';
+
+// import barChart
+import { BarChart } from 'react-native-chart-kit';
+
+// import dropdown picker
+import DropDownPicker from 'react-native-dropdown-picker';
+
+// import async storage
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+// import axios
+import axios from 'axios';
 
 export default function HistoryScreen() {
+  // init dimension
+  const screenHeight = Dimensions.get('window').height;
+  const screenWeight = Dimensions.get('window').width;
+
+  // init state for dropdown
+  const [open, setOpen] = useState(false);
+  const [value, setValue] = useState(1);
+  const [items, setItems] = useState([
+    {label: '1 Hari', value: 1},
+    {label: '3 Hari', value: 3},
+    {label: '7 Hari', value: 7}
+  ]);
+
+  // state for data chart
+  const [labels, setLabels] = useState([]);
+  const [total, setTotal] = useState([]);
+
+  const getHistories = async () => {
+    try {
+      const ipServer = await AsyncStorage.getItem('IpServer');
+      if (ipServer !== null) {
+        await axios
+        .get(`${ipServer}/api/histories?periode=${value}`)
+        .then(response => {
+          // set data to state
+          setLabels(response.data.data.labels);
+          setTotal(response.data.data.total);
+        });
+      }
+    } catch (e) {
+      // error reading value
+      console.log(e)
+    }
+  };
+
+  // hook useEffect
+  useEffect(() => {
+    // call function getHistories
+    getHistories();
+  }, [value]);
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>History Screen</Text>
-    </View>
+    <SafeAreaView style={{ padding: 15 }}>
+      <View 
+        style={{ 
+          flex: 1,
+          alignItems: 'center',
+          justifyContent: 'center',
+          marginTop: 60,
+        }}>
+        <View>
+            <DropDownPicker
+              open={open}
+              value={value}
+              items={items}
+              setOpen={setOpen}
+              setValue={setValue}
+              setItems={setItems}
+              placeholder='-- Pilih Periode --'
+            />
+          </View>
+          <View style={{ marginBottom: 50 }}></View>
+          <View style={{ padding: 20 }}>
+            <BarChart 
+            data={{ 
+              labels: labels,
+              datasets: [
+                  {
+                    data: total
+                  },
+                ],
+              }}
+              width={screenWeight / 1.1}  // from react-native
+              height={screenHeight / 1.7}
+              yAxisInterval={1} // optional, default to 1
+              chartConfig={{ 
+                  backgroundColor: '#405ff3',
+                  backgroundGradientFrom: '#505ff4',
+                  backgroundGradientTo: '#205ff3',
+                  decimalPlaces: 2, // optional, defaults to 2dp
+                  color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+                  labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+                  style: {
+                    borderRadius: 16,
+                  },
+                  propsForDots: {
+                    r: '6',
+                    strokeWidth: '2',
+                    stroke: '#ffa726',
+                  },
+              }}
+              bezier
+              style={{ 
+                marginVertical: 8,
+                borderRadius: 16,
+              }}
+              verticalLabelRotation={30}
+            />
+          </View>
+        </View>
+    </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-
-  text: {
-    fontSize: 20,
-    fontWeight: 'bold',
-    textAlign: 'center',
-    color: '#000',
-  },
-});
+const styles = StyleSheet.create({});
--- src/screens/lamps/Index.jsx
+++ src/screens/lamps/Index.jsx
@@ -1,27 +1,197 @@
-import {View, Text, StyleSheet} from 'react-native';
+import {
+  SafeAreaView,
+  View,
+  Text,
+  ScrollView,
+  FlatList,
+  StyleSheet,
+  TouchableOpacity,
+  Image,
+  ToastAndroid,
+} from 'react-native';
 
-import React from 'react';
+// import hook
+import React, { useState, useEffect } from 'react';
+
+// import async storage
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+// import axios
+import axios from 'axios';
+
+// import switch
+import { Switch } from 'react-native-switch';
 
 export default function LampScreen() {
+  // init state lamps
+  const [lamps, setLamps] = useState([]);
+
+  const getLamps = async () => {
+    try {
+      const ipServer = await AsyncStorage.getItem('IpServer');
+      if (ipServer !== null) {
+        // get status lampu from server
+        await axios.get(`${ipServer}/api/lamps`).then(response => {
+          // set data lampu
+          setLamps(response.data.data);
+        });
+      }
+    } catch (e) {
+      // error reading value
+      console.log(e)
+    }
+  };
+
+  // hook useEffect
+  useEffect(() => {
+    // call function getLamps
+    getLamps();
+  }, []);
+
+  const toggleSwitch = async id => {
+    // update to database
+    try {
+      // get ip server from async storage
+      const ipServer = await AsyncStorage.getItem('IpServer');
+      if (ipServer !== null) {
+        // get status lampu from server
+        await axios.post(`${ipServer}/api/lamps/${id}`).then(response => {
+          // call function getLamps
+          getLamps();
+          ToastAndroid.show(`${response.data.message}`, ToastAndroid.SHORT);
+        });
+      }
+    } catch (e) {
+      // error reading value
+      console.log(e)
+    }
+  }
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Lamp Screen</Text>
-    </View>
+    <SafeAreaView>
+      <ScrollView style={{ padding: 15 }}>
+        <View style={{ flex: 1, flexDirection: 'row' }}>
+          <FlatList
+            style={{ flex: 1, marginTop: 10, marginBottom: 260 }}
+            data={lamps}
+            renderItem={({ item, index, separators }) => (
+              <TouchableOpacity style={styles.container} onPress={() => { }}>
+                {item.status === 1 && (
+                  <Image
+                    source={require('../../assets/images/on.png')}
+                    style={{
+                      width: '25%',
+                      aspectRatio: 5 / 5,
+                      borderRadius: 5,
+                      marginRight: 15,
+                      padding: 50,
+                      backgroundColor: '#A5D6A7',
+                    }}
+                  />
+                )}
+                {item.status === 0 && (
+                  <Image
+                    source={require('../../assets/images/off.png')}
+                    style={{
+                      width: '25%',
+                      aspectRatio: 5 / 5,
+                      borderRadius: 5,
+                      marginRight: 15,
+                      padding: 50,
+                      backgroundColor: '#EEEEEE',
+                    }}
+                  />
+                )}
+                <View style={styles.block}>
+                  <Text
+                    style={{
+                      color: '#333333',
+                      fontWeight: 'bold',
+                      fontSize: 16,
+                      marginTop: 10,
+                    }}>
+                    {item.name}
+                  </Text>
+                  <View
+                    style={{
+                      marginTop: 10,
+                      height: 1,
+                      backgroundColor: '$ddd',
+                      alignSelf: 'stretch'
+                    }}></View>
+                  <View
+                    style={{
+                      flexDirection: 'row',
+                      justifyContent: 'space-between',
+                      marginTop: 15,
+                      marginLeft: 10,
+                    }}>
+                      <Switch 
+                        trackColor={{ false: '#767577', true: '#81b0ff' }}
+                        thumbColor={item.status ? '#f55d4b' : '#f4f3f4'}
+                        onValueChange={toggleSwitch.bind(null, item.id)}
+                        value={item.status === 1 ? true : false}
+                      />
+                    </View>
+                </View>
+              </TouchableOpacity>
+            )}
+            eyExtractor={item => item.id}
+            scrollEnabled={false}
+          />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  labelContainer: {
+    marginTop: 5,
+    flexDirection: 'row',
   },
 
-  text: {
-    fontSize: 20,
+  labelIcon: {
+    marginRight: 5,
+    color: '#333333',
+  },
+
+  labelText: {
+    color: '#333333',
     fontWeight: 'bold',
-    textAlign: 'center',
-    color: '#000',
+  },
+
+  container: {
+    flex: 1,
+    marginTop: 10,
+    marginBottom: 20,
+  },
+
+  container: {
+    flexDirection: 'row',
+    marginBottom: 10,
+    backgroundColor: '#fff',
+    padding: 7,
+    borderRadius: 10,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+
+    shadowOpacity: 0.18,
+    shadowRadius: 1.0,
+    elevation: 2,
+  },
+
+  image: {
+    width: '35%',
+    aspectRatio: 5 / 5,
+    marginRight: 8,
+  },
+
+  block: {
+    padding: 0,
+    width: '65%',
   },
 });
